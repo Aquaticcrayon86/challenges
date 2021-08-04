f92d0a0a1e4c35788d8a8eae401e1a0949b30977
Aquaticcrayon86 <88463258+Aquaticcrayon86@users.noreply.github.com>
2021-08-04 17:36:37 -0500

    Add files via upload

---
+++ challenge-calculate the area of a triangle.py
@@ -0,0 +1,11 @@
+#ask for base
+print("say the base of the triangle")
+x = input()
+#ask for hight
+print("say the height of the triangle")
+y = input()
+#multiply together
+mult = int(x) * (y)
+#divide by 2
+divmult = mult
+div = divmult / 2
